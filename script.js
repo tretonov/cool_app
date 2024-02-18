093e0c5f72ea98264057bf242cf4210b94978530
tretonov <152734801+tretonov@users.noreply.github.com>
2024-02-18 16:55:04 +0300

    Update and rename script.js to init.html

--- script.js
+++
@@ -1,6 +0,0 @@
-fetch('https://run.mocky.io/v3/your-unique-id')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('number').innerText = data.number;
-  })
-  .catch(error => console.error('Error:', error));
